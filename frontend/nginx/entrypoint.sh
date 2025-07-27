456c7d4f0554efdc856dc32f16c9e429b575f158
DanielFreitassc <danielfreitassc@gmail.com>
2025-07-27 02:02:38 -0300

    Refactor: trocando caminhos para o cert

--- frontend/nginx/entrypoint.sh
+++ frontend/nginx/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-CERT_PATH="/cert/fullchain.pem"
-KEY_PATH="/cert/privkey.pem"  
+
+CERT_PATH="/etc/nginx/certs/fullchain.pem"
+KEY_PATH="/etc/nginx/certs/privkey.pem"
 
 if [ ! -f "$CERT_PATH" ] || [ ! -f "$KEY_PATH" ]; then
   echo "🔐 Gerando certificado autoassinado..."
   openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout "$KEY_PATH" \
     -out "$CERT_PATH" \
-    -subj "/C=BR/ST=SP/L=SaoPaulo/O=MinhaEmpresa/OU=Dev/CN=localhost" \
+    -subj "/C=BR/ST=SC/L=SantaCatarina/O=EnVault/OU=Dev/CN=localhost" \
     -addext "subjectAltName = DNS:localhost,IP:127.0.0.1"
 else
   echo "✅ Certificado já existe. Pulando geração."
