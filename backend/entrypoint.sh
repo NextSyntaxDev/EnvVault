ea4a27adbcccc78ea391a915a3539531b7e1c098
DanielFreitassc <danielfreitassc@gmail.com>
2025-07-27 02:46:30 -0300

    Refactor: script não usado

--- backend/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd target || { echo "Erro: Pasta 'target' não encontrada."; exit 1; }
-java -Xms256m -Xmx768m -jar backend-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod > /dev/null 2>&1 &
