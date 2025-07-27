6aedcb992f5b9665764e46a4f88831d413df6633
DanielFreitassc <danielfreitassc@gmail.com>
2025-07-27 00:56:19 -0300

    Feat: processo de automatização de certificado auto assinado

--- frontend/nginx/entrypoint.sh
+++ frontend/nginx/entrypoint.sh
@@ -1,15 +1,17 @@
-CERT_PATH=/cert/fullchain.pem
-KEY_PATH=/cert/privkey.pem
+#!/bin/sh
+CERT_PATH="/cert/fullchain.pem"
+KEY_PATH="/cert/privkey.pem"  
 
 if [ ! -f "$CERT_PATH" ] || [ ! -f "$KEY_PATH" ]; then
-  echo "🔐 Gerando certificado autoassinado como fullchain.pem e privkey.pem..."
+  echo "🔐 Gerando certificado autoassinado..."
   openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout "$KEY_PATH" \
     -out "$CERT_PATH" \
-    -subj "/C=BR/ST=SP/L=SaoPaulo/O=MyCompany/OU=Dev/CN=localhost"
+    -subj "/C=BR/ST=SP/L=SaoPaulo/O=MinhaEmpresa/OU=Dev/CN=localhost" \
+    -addext "subjectAltName = DNS:localhost,IP:127.0.0.1"
 else
   echo "✅ Certificado já existe. Pulando geração."
 fi
 
-# Inicia o Nginx
-nginx -g 'daemon off;'
+chmod 600 "$KEY_PATH"
+exec "$@"
